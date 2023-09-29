f87f1ecc469f99b599756a34709ab7770b9d588b
hayao <hayao@fascode.net>
2023-09-29 12:46:42 +0900

    Add: mirakurun-playlist.sh

---
+++ mirakurun-playlist/README.md
@@ -0,0 +1,9 @@
+## Mirakurun Playlist Generator
+
+MirakurunのAPIからVLC Media Playerで使えるプレイリストを生成します。
+
+### 参考
+
+- [Raspberry Pi 4で64ビットMirakurun DVRチューナサーバを動かしてみました \| 日記というほどでも](https://denor.jp/raspberry-pi-4%E3%81%A764%E3%83%93%E3%83%83%E3%83%88mirakurun-dvr%E3%83%81%E3%83%A5%E3%83%BC%E3%83%8A%E3%82%B5%E3%83%BC%E3%83%90%E3%82%92%E5%8B%95%E3%81%8B%E3%81%97%E3%81%A6%E3%81%BF%E3%81%BE%E3%81%97#VLC)
+- [Mirakurun/api\.yml at master · Chinachu/Mirakurun](https://github.com/Chinachu/Mirakurun/blob/master/api.yml)
+- [mirakurun を使って VLC でテレビを視聴する。 \| 妄想日記 by 妄想エンジン](https://www.mousou.org/node/428)
---
+++ mirakurun-playlist/mirakurun-playlist.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+#-- Configs --#
+# Server
+mk_api_services="/api/services"
+
+# m3u8 playlist
+# %CH%: 物理的なチャンネル番号
+# %SEVICE%: serviceId
+# %CH_TYPE%: GR | CS | BS
+# %CH_TYPE_JP%: 地上波 | CS | BS
+# %CH_NAME%: チャンネル名
+# %MK_IP%: Miraurun IP
+pl_m3u8_header="#EXTM3U\n#EXTVLCOPT:network-caching=1000"
+pl_m3u8_content="#EXTINF:-1,%CH_TYPE_JP% - %CH_NAME%\nhttp://%MK_IP%/api/channels/%CH_TYPE%/%CH%/services/%SERVICE%/stream/"
+
+
+#-- Script config --#
+set -e -u -E -o pipefail
+
+#-- Set up global vars --#
+: "${pl_path:="./tv_playlist.m3u8"}"
+: "${mk_ip:="localhost"}"
+: "${mk_port:="40772"}"
+
+
+#-- Functions --#
+# get_from_json <variable name> <jq args...>
+# example: get_from_json api_services
+get_from_json(){
+    local _var="$1"
+    shift 1 || true
+    jq -r "$@" <<< "$(eval "echo \"\${${_var}}\"")" || return $?
+}
+
+# get_json <entry point>
+get_json(){
+    curl --no-progress-meter -L -f "http://$mk_ip:$mk_port${1}"
+}
+
+chtype_to_text(){
+    case "$1" in
+        "GR") 
+            echo "地上波"
+            ;;
+        *)
+            echo "$1"
+            ;;
+    esac
+}
+
+# parse_template VAR=VALUE
+# stdin: text
+parse_template(){
+    local _sed_args=() i
+    for i in "$@"; do
+        _sed_args+=("-e" "s|%$(cut -d "=" -f 1 <<< "$i")%|$(cut -d "=" -f 2 <<< "$i")|g")
+    done
+    sed "${_sed_args[@]}"
+}
+
+stdin_echo(){
+    xargs echo -e
+}
+
+scirpt_usage(){
+    cat << "EOF"
+Generate m3u8 playlist from Mirakurun API
+
+Usage: $0 [IP]
+
+Options:
+    -i | --ip IP      Specify mirakurun IP address  (DEFAULT=localhost)
+    -p | --port PORT  Specify mirakurun port (DEFAULT=40772)
+    -l | --list FILE  Specify the path of the playlist to ganarate
+    -h | --help       Show this help message.
+EOF
+}
+
+
+
+#-- Entry point --#
+main(){
+    local mk_api_services_json=""
+
+    _parse_args(){
+        local _no_args=()
+
+        # usage: hasarg "${2}"
+        hasarg(){
+            [[ "${1-""}" ]] || {
+                echo "Spedify args. Please reas the usage."
+                exit 1
+            }
+        }
+
+        while [[ -n "${1-""}" ]]; do
+            case "$1" in
+                "-l" | "--list")
+                    hasarg "${2-""}" && pl_path="$2"
+                    shift 2
+                    ;;
+                "-p" | "--port")
+                    hasarg "${2-""}" && mk_port="$2"
+                    shift 2
+                    ;;
+                "-i" | "--ip")
+                    hasarg "${2-""}" && mk_ip="$2"
+                    shift 2
+                    ;;
+                "-h" | "--help")
+                    scirpt_usage
+                    exit 0
+                    ;;
+                *)
+                    _no_args+=("$1")
+                    shift 1
+                    ;;
+            esac
+        done
+
+        mk_ip="${_no_args[0]:-"$mk_ip"}"
+    }
+
+    _prepare_json(){
+        mk_api_services_json="$(get_json "$mk_api_services")"
+    }
+
+
+    _generate_playlist(){
+        echo "Generating playlist to $pl_path ..."
+        
+        {
+            echo -e "$pl_m3u8_header"
+            while read -r json_per_ch; do
+                echo -e "$(parse_template \
+                    MK_IP="$mk_ip" \
+                    CH="$(get_from_json json_per_ch ".channel.channel")" \
+                    SERVICE="$(get_from_json json_per_ch ".serviceId")" \
+                    CH_TYPE="$(get_from_json json_per_ch ".channel.type")" \
+                    CH_TYPE_JP="$(chtype_to_text "$(get_from_json json_per_ch ".channel.type")")" \
+                    CH_NAME="$(chtype_to_text "$(get_from_json json_per_ch ".name")")" \
+                    <<< "$pl_m3u8_content")"
+            done < <(get_from_json mk_api_services_json -c ".[]")
+        } > "$pl_path"
+    }
+
+    _parse_args "$@"
+    _prepare_json
+    _generate_playlist
+}
+
+#-- Start --#
+main "$@"
