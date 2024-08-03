6b69a1c764649d5ce3e9d8df1fa3b5fce59d6ea4
hayao <shun819.mail@gmail.com>
2024-08-03 09:40:08 +0900

    fix

--- univ/src/clang-kato/5.c
+++ univ/src/clang-kato/5.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main()
+{
     char s1[20] = "abcd";
     char s2[20] = "abcD";
-    int x,y;
+    int x, y;
     x = strncmp(s1, s2, 4);
-    y = strncmp(s1,s2,3);
-    printf("%d,%d\n");
+    y = strncmp(s1, s2, 3);
+    printf("%d,%d\n", x, y);
     return 0;
 }
