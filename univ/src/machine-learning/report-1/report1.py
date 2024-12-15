adad14ad3ed83c2a7a9257d57c0e08cb622d8ef4
hayao <hayao@fascode.net>
2024-12-15 20:47:40 +0900

    fix

--- univ/src/machine-learning/report-1/report1.py
+++ univ/src/machine-learning/report-1/report1.py
@@ -275,7 +275,7 @@ def graphPclass() -> None:
 
 
 def main() -> ExitCode:
-    match sys.argv[1] if len(sys.argv) == 1 else "":
+    match sys.argv[1] if len(sys.argv) != 1 else "":
         case "prepared-info":
             preparedInfoCmd()
         case "completed-info":
